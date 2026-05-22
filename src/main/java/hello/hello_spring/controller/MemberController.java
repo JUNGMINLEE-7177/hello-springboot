6249f6a87fceb749a98f04af01372a5fe6a9557d
JUNGMIN-7177 <min111700@naver.com>
2026-05-22 16:30:58 +0900

    5장 수정

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,20 +1,24 @@
 package hello.hello_spring.controller;
 
+
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
 
-    private final MemberService memberServcice;
+    private final MemberService memberService;
 
     @Autowired
     public MemberController(MemberService memberServcice) {
-        this.memberServcice = memberServcice;
+        this.memberService = memberServcice;
     }
 
     @GetMapping(value = "/members/new")
@@ -27,10 +31,15 @@ public class MemberController {
         Member member = new Member();
         member.setName(form.getName());
 
-        memberServcice.join(member);
+        memberService.join(member);
 
         return "redirect:/";
+    }
 
-
+    @GetMapping(value = "/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
