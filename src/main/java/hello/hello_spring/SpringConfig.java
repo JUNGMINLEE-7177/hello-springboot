662d8617ded4f135f3f9775ea07341892425d790
JUNGMINLEE-7177 <min111700@naver.com>
2026-05-28 09:32:19 +0900

    jdbc 추가

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,13 +1,28 @@
 package hello.hello_spring;
 
+import hello.hello_spring.repository.JdbcMemberRepository;
+import hello.hello_spring.repository.JpaMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import hello.hello_spring.service.MemberService;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource, EntityManager em){
+        this.dataSource = dataSource;
+        this.em = em;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +31,10 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
+
     }
 }
